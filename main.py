4dad2e247f24cd25d79dfd67221a1e16e508bfe4
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-04-25 15:04:09 +0900

    change: cogを読み込むようにした。

--- main.py
+++ main.py
@@ -11,9 +11,33 @@ from ujson import load
 with open("secret.json") as f:
     secret = load(f)
 
-bot = commands.Bot("rs!", help_command=None)
+intents = discord.Intents.all()
+bot = commands.Bot(
+    command_prefix="rs!",
+    activity=discord.Activity(
+        type=discord.ActivityType.watching,
+        name=f"起動準備",
+    ),
+    application_id=967729996221259836,
+    status=discord.Status.dnd,
+    intents=intents,
+    help_command=None,
+)
 bot.secret = secret
 
+@bot.listen()
+async def setup_hook():
+    await bot.load_extension("cogs.*")
+    await bot.load_extension("jishaku")
+
+@bot.listen()
+async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name=f"free RT Server",
+        ),
+    )
 
 async def main():
     await bot.start(bot.secret["TOKEN"])
